ec6d3b56f3ee1f5abd81b3d4d31c2112a266bed3
Aditya Sharma <adityasharmasder@gmail.com>
2026-07-21 17:34:54 +0530

    Add Pomodoro timer with start, pause, reset and auto-stop functionality

--- JS/app.js
+++ JS/app.js
@@ -1,52 +1,51 @@
 
 
-const MenuItems = document.querySelectorAll(".menu li");
-const menuBtn = document.querySelector(".menu-btn");
-const sidebar = document.querySelector("aside");
-const addBtn = document.querySelector(".add-btn");
-const tasksection = document.querySelector(".task-section");
 
-let completed = 0;
+const menuBtn = document.querySelector(".menu-btn");
+const sidebAR = document.querySelector("aside");
+const menuItems = document.querySelectorAll(".menu li")
+const addBtn = document.querySelector(".add-btn");
+const taskSection = document.querySelector(".task-section")
+
+let complete = 0;
+
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
 
-
-
-// Menu Bar Features
-MenuItems.forEach((item) => {
+//features of sidebar 
+menuItems.forEach((item) => {
     item.addEventListener("click", () => {
-        MenuItems.forEach((menu) => {
+        menuItems.forEach((menu) => {
             menu.classList.remove("active");
-        });
+        })
         item.classList.add("active");
     });
 });
 
+
 menuBtn.addEventListener("click", () => {
-    sidebar.classList.toggle("close")
+    sidebAR.classList.toggle("close");
+
 });
 
+tasks.forEach((taskData, index) => {
+    createTask(taskData, index);
+});
+
+updateTotalTask();
+
+function createTask(taskData, index) {
 
 
-//task section and features
-function CreateTaskSection() {
 
-    tasks.forEach((taskData, index) => {
-        createTask(taskData, index);
-
-    })
-    updateTotalTask()
-
-    function createTask(taskData, index) {
-        //task item
-        const task = document.createElement("div");
-        task.className = "task-item"
-        task.innerHTML = ` <div class="task-left">
+    const task = document.createElement("div")
+    task.className = "task-item";
+    task.innerHTML = ` <div class="task-left">
                             <input type="checkbox">
 
                             <div>
                                 <h4>${taskData.taskName}</h4>
-                                
+
                             </div>
                         </div>
 
@@ -57,120 +56,158 @@ function CreateTaskSection() {
                         </div>
                     `
 
-
-        // tasksection.insertBefore(task, tasksection.children[1])
-        tasksection.appendChild(task);
+    taskSection.appendChild(task);
 
 
-        //delete button features
-        const deleteBtn = task.querySelector(".delete-button");
+    const deleteBtn = task.querySelector(".delete-button");
 
-        deleteBtn.addEventListener("click", () => {
-            if (checkbox.checked) {
-                completed--;
-                completedCount.textContent = completed
-            };
-            task.remove()
-            tasks.splice(index, 1);
-            localStorage.setItem("tasks", JSON.stringify(tasks));
-            updateTotalTask();
+    deleteBtn.addEventListener("click", () => {
+        if (checkbox.checked) {
+            complete--;
+            completedCount.textContent = complete;
+        }
 
-        });
+        task.remove();
+        tasks.splice(index, 1);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        updateTotalTask();
+
+    })
 
 
-        //checkbox features
-        const checkbox = task.querySelector("input");
-        const completedCount = document.querySelector(".completed-count");
-        checkbox.addEventListener("change", () => {
-            if (checkbox.checked) {
-                task.classList.add("completed");
 
-                completed++;
-                completedCount.textContent = completed;
-                taskData.completed = true
-
-            }
-            else {
-                task.classList.remove("completed");
-
-                completed--;
-                completedCount.textContent = completed;
-                taskData.completed = false
-            }
-            localStorage.setItem("tasks", JSON.stringify(tasks))
-
-        });
-
-        if (taskData.completed) {
-            checkbox.checked = true;
+    const checkbox = task.querySelector("input")
+    const completedCount = document.querySelector(".completed-count");
+    checkbox.addEventListener("change", () => {
+        if (checkbox.checked) {
             task.classList.add("completed");
 
-            completed++;
-            completedCount.textContent = completed;
-        }
-    };
-
-
-
-    //add button karne pe task section ka behave
-    addBtn.addEventListener("click", () => {
-        const taskName = prompt("Entre task Name");
-        // const priority = prompt("priority");
-
-        if (taskName === "" || taskName === null) {
-            alert("Please Enter Task First");
-            return;
+            complete++;
+            completedCount.textContent = complete;
+            taskData.complete = true;
         }
 
+        else {
+            task.classList.remove("completed")
 
-        const now = new Date();
-        const hour = now.getHours();
-        const minute = now.getMinutes();
-        const currentTime = `${hour} : ${minute}`
-
-
-        //task Data Object
-        const taskData = {
-            taskName,
-            currentTime,
-            completed: false
+            complete--;
+            completedCount.textContent = complete;
+            taskData.complete = false;
         }
 
-        tasks.push(taskData)
         localStorage.setItem("tasks", JSON.stringify(tasks));
-        createTask(taskData, tasks.length - 1);
-        updateTotalTask();
+
     });
+
+    if (taskData.complete) {
+        checkbox.checked = true;
+        task.classList.add("completed");
+
+        complete++;
+        completedCount.textContent = complete;
+    }
 
 
 }
-CreateTaskSection();
-
-//task section searchbar features
-const searchTask = document.querySelector(".search-task");
-
-searchTask.addEventListener("input", () => {
-
-    const allTask = document.querySelectorAll(".task-item");
-
-    allTask.forEach((task) => {
-
-        const taskTitle = task.querySelector("h4");
 
 
-        if (taskTitle.textContent
-            .toLocaleLowerCase()
-            .includes(searchTask.value.toLowerCase())) {
-            task.style.display = "flex";
-        }
-        else {
-            task.style.display = "none";
-        }
-    })
+
+
+addBtn.addEventListener("click", () => {
+    const taskName = prompt("Entre task Name")
+
+    if (taskName === "" || taskName === null) {
+        alert("Please Entre The Task First");
+        return;
+    }
+
+
+    const now = new Date();
+    const hour = now.getHours();
+    const minute = now.getMinutes();
+    const currentTime = `${hour} : ${minute}`
+
+    const taskData = {
+        taskName,
+        currentTime,
+        complete: false
+    }
+
+    tasks.push(taskData);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    createTask(taskData, tasks.length - 1);
+    updateTotalTask();
+
 });
 
 
 function updateTotalTask() {
-    const totalTaskCount = document.querySelector(".total-task-count");
-    totalTaskCount.textContent = tasks.length;
+    const totalTaskContent = document.querySelector(".total-task-count");
+    totalTaskContent.textContent = tasks.length;
 }
+
+const searchTask = document.querySelector(".search-task");
+
+searchTask.addEventListener("input" , ()=>{
+    const allTask = document.querySelectorAll(".task-item");
+
+    allTask.forEach((task) =>{
+         const taskTitle = task.querySelector("h4");
+         if (taskTitle.textContent.toLocaleLowerCase()
+            .includes(searchTask.value.toLowerCase())){
+            task.style.display = "flex";
+         }
+         else {
+            task.style.display = "none";
+            
+         }
+        
+    });
+})
+
+const timer = document.querySelector(".timer");
+const startBtn = document.querySelector(".start-btn");
+const resetBtn = document.querySelector(".reset-btn");
+const pauseBtn = document.querySelector(".pause-btn");
+
+let timeLeft = 25*60;
+let interval;
+
+function updateTimer(){
+    const minute = Math.floor(timeLeft / 60);
+    let second = (timeLeft % 60);
+
+    if (second < 10){
+        second = "0" + second;
+    }
+
+    timer.textContent = `${minute} : ${second}`;
+};
+
+startBtn.addEventListener("click" , ()=>{
+        clearInterval(interval);
+        interval = setInterval(() =>{
+            timeLeft--;
+            updateTimer();
+
+            if (timeLeft <= 0){
+                clearInterval(interval)
+                alert("Promodoro Complete");
+                timeLeft = 25*60;
+                updateTimer();
+            }
+            
+        }, 1000);
+
+});
+
+resetBtn.addEventListener("click" , ()=>{
+        clearInterval(interval);
+        timeLeft = 25*60;
+        updateTimer();
+       
+});
+
+pauseBtn.addEventListener("click", ()=>{
+    clearInterval(interval);
+});
